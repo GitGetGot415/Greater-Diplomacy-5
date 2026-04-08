35e3a1b455bb952bab2cf2ca0d71ba821a4c3283
GitGetGot415 <55815440+GitGetGot415@users.noreply.github.com>
2026-04-07 19:23:03 -0700

    commented out some buttons so as to narrow the scope of what should be focused on

--- screens/new_game.py
+++ screens/new_game.py
@@ -14,8 +14,8 @@ class New_Game(GameState):
         self.elements = [
             Button(50, 50, "small", "red", "Back", self.exit_to_menu),
             # Added the Random button here
-            Button("centered", 130, "large", "orange", "RANDOM SCENARIO", self.start_random_scenario),
-            Button("centered", "centered", "big", "red", "Check map tools", self.map_selected),
+            Button("centered", "centered", "large", "orange", "RANDOM SCENARIO", self.start_random_scenario),
+            # Button("centered", "centered", "big", "red", "Check map tools", self.map_selected),
         ]
         
         # Look for scenarios in the scenarios folder
@@ -23,14 +23,14 @@ class New_Game(GameState):
         if not os.path.exists(scenario_dir):
             os.makedirs(scenario_dir)
             
-        scenarios = os.listdir(scenario_dir)
+        """scenarios = os.listdir(scenario_dir)
         for i, name in enumerate(scenarios):
             btn_y = 200 + (i * 70)
             # Create a button for each scenario
             self.elements.append(
                 Button("centered", btn_y, "large", "blue", name, 
                        lambda n=name: self.start_scenario(n))
-            )
+            )"""
 
     def map_selected(self):
         self.next_state = "MAP"
