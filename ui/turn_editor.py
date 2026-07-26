a09582e6d7f82af2ae16f376cf91c927d4ef489c
GitGetGot415 <55815440+GitGetGot415@users.noreply.github.com>
2026-07-25 23:21:55 -0700

    construction turn editor now works for units with high roman numerals

--- ui/turn_editor.py
+++ ui/turn_editor.py
@@ -52,7 +52,7 @@ class TurnEditor:
         if " Lvl " in name:
             return name.split(" Lvl ")[0]
         parts = name.split(" ")
-        if parts[-1] in ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"]:
+        if parts[-1] in ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX", "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL", "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L"]:
             return " ".join(parts[:-1])
         return name
 
