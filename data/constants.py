a8b0dfe0cd85631a7c75af7f54fa23a8b0d07ce6
GitGetGot415 <55815440+GitGetGot415@users.noreply.github.com>
2026-08-04 22:26:38 -0700

    awesome another update

--- data/constants.py
+++ data/constants.py
@@ -116,7 +116,7 @@ SHOW_FPS = False
 # expect it.
 CHECKERBOARD_WATER = False
 
-GAME_VERSION = "v23"
+GAME_VERSION = "v24"
 VERSION_CHECK_URL = "https://raw.githubusercontent.com/GitGetGot415/Greater-Diplomacy-5/main/version.txt"
 
 # --- TACTICAL MODE CONSTANTS ---
