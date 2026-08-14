5c73a06b7cf27ac58086d63ce95c87b27abb8d26
GitGetGot415 <55815440+GitGetGot415@users.noreply.github.com>
2026-08-14 16:47:11 +0200

    fuel upkeep decreased to 40%

--- data/constants.py
+++ data/constants.py
@@ -67,7 +67,7 @@ BERGIUS_FUEL_BONUS = 100
 UPKEEP_MODIFIERS = {
     "manpower": 0.10,
     "materials": 0.05,
-    "fuel": 0.50
+    "fuel": 0.40
 }
 
 # reminder that base days per turn is the scenario default
